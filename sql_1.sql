d5ae69501016087ed94af8df2eff3865aa8c42e5
HZhalex <97648418+HZhalex@users.noreply.github.com>
2025-03-22 07:31:37 +0700

    Add files via upload

--- sql_1.sql
+++ sql_1.sql
@@ -618,9 +618,24 @@ having count(num) = 1
 )
 as hung;
 
+-- bai 30:
+drop database hung;
+create database hung;
+use hung;
+Create table If Not Exists Employees(employee_id int, name varchar(20), reports_to int, age int);
+Truncate table Employees;
+insert into Employees (employee_id, name, reports_to, age) values ('9', 'Hercy', NULL, '43');
+insert into Employees (employee_id, name, reports_to, age) values ('6', 'Alice', '9', '41');
+insert into Employees (employee_id, name, reports_to, age) values ('4', 'Bob', '9', '36');
+insert into Employees (employee_id, name, reports_to, age) values ('2', 'Winston', NULL, '37');
 
-
-
+select e1.employee_id employee_id,e1.name name,count(e2.employee_id) reports_count ,round(avg(e2.age)) average_age
+from employees e1
+join employees e2
+on e1.employee_id = e2.reports_to
+group by e1.employee_id,e1.name
+ORDER BY e1.employee_id
+;
 
 
 
