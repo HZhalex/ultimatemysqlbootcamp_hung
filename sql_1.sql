bd404ed1c7857a7628371300992d1017fdd6bcb1
HZhalex <97648418+HZhalex@users.noreply.github.com>
2025-03-28 19:26:35 +0700

    Add files via upload

--- sql_1.sql
+++ sql_1.sql
@@ -693,5 +693,75 @@ with hung as (
 select distinct num as ConsecutiveNums  from hung where num = num1 and num = num2;
 
 
+-- bai 33:
+drop database hung;
+create database hung;
+use hung;
+Create table If Not Exists Employees (employee_id int, name varchar(20), manager_id int, salary int);
+Truncate table Employees;
+insert into Employees (employee_id, name, manager_id, salary) values ('3', 'Mila', '9', '60301');
+insert into Employees (employee_id, name, manager_id, salary) values ('12', 'Antonella', NULL, '31000');
+insert into Employees (employee_id, name, manager_id, salary) values ('13', 'Emery', NULL, '67084');
+insert into Employees (employee_id, name, manager_id, salary) values ('1', 'Kalel', '11', '21241');
+insert into Employees (employee_id, name, manager_id, salary) values ('9', 'Mikaela', NULL, '50937');
+insert into Employees (employee_id, name, manager_id, salary) values ('11', 'Joziah', '6', '28485');
+
+select * from employees;
+select e1.employee_id from employees e1 left join employees e2 
+on e1.manager_id = e2.employee_id
+where e1.salary < 30000 and e2.employee_id IS NULL and e1.manager_id IS NOT NULL
+ORDER BY employee_id;
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
