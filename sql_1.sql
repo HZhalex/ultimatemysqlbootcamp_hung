02c8e02248993275e70c5794a15f0b99a23dca5c
HZhalex <97648418+HZhalex@users.noreply.github.com>
2025-03-30 15:27:11 +0700

    Add files via upload

--- sql_1.sql
+++ sql_1.sql
@@ -714,8 +714,19 @@ ORDER BY employee_id;
 ;
 
 
+-- bai 34:
+drop database hung;
+create database hung;
+use hung;
+Create table If Not Exists Users (user_id int, name varchar(40));
+Truncate table Users;
+insert into Users (user_id, name) values ('1', 'aLice');
+insert into Users (user_id, name) values ('2', 'bOB');
 
-
+SELECT Users.user_id , concat(upper(substr(users.name,1,1)),lower(substr(users.name,2))) AS name 
+FROM Users
+ORDER BY
+Users.user_id ASC;
 
 
 
